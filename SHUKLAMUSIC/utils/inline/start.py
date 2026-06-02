e7299f23b65782a3126121be4f4fb1dff22f56ed
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-02 22:47:04 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -22,7 +22,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true", style=ButtonStyle.DANGER),
+                url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper", style=ButtonStyle.SUCCESS),
