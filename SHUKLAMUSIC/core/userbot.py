196071ed05752731707bacee11508f369de330bd
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-02 18:38:52 +0530

    Update userbot.py

--- SHUKLAMUSIC/core/userbot.py
+++ SHUKLAMUSIC/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ITSZSHUKLA")
-                await self.one.join_chat("MASTIWITHFRIENDSXD")
+                await self.one.join_chat("ll_DPZ_WORLDS_lll")
+                await self.one.join_chat("ll_DPZ_WORLDS_lll")
             except:
                 pass
             assistants.append(1)
