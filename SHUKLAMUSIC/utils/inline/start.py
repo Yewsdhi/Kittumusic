cf4792a73318f8060f2e9fcc017091102a6ccffb
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-02 19:33:20 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -1,6 +1,5 @@
-from pyrogram.enums import ButtonStyle
 from pyrogram.types import InlineKeyboardButton
-
+from pyrogram.enums import ButtonStyle
 import config
 from SHUKLAMUSIC import app
 
@@ -28,10 +27,10 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=ButtonStyle.SUCCESS,
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style=ButtonStyle.SUCCESS,
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style=ButtonStyle.SUCCESS),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=ButtonStyle.SUCCESS,
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=ButtonStyle.SUCCESS),
         ],
     ]
     return buttons
