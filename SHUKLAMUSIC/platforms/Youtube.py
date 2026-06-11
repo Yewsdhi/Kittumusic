b94a1203104f3d91e939eaabedd32263c371575b
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-11 08:38:21 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -17,7 +17,7 @@ import httpx
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 
-from AloneMusic import LOGGER
+from SHUKLAMUSIC import LOGGER
 
 # Use environment variables for configuration
 API_URL = os.getenv("API_URL", "https://web.riteshyt.in").rstrip("/")
